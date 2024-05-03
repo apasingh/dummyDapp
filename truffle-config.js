b3c14b4ce09443d75b53896e554b116a11b4f744
apasingh <104095503+apasingh@users.noreply.github.com>
2024-05-03 15:28:00 -0400

    now it at least gets to the compiling properly

--- truffle-config.js
+++ truffle-config.js
@@ -47,23 +47,35 @@ const { MNEMONIC, PROJECT_ID } = process.env;
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a managed Ganache instance for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
 
+module.exports = {
   networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8000,
-      network_id: '11155111', // Match any network id
-    },
+    sepolia: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      // Other optional configurations...
+    }
+    // Other network configurations...
+    
+  // Other Truffle configurations...
+// module.exports = {
+//   /**
+//    * Networks define how you connect to your ethereum client and let you set the
+//    * defaults web3 uses to send transactions. If you don't specify one truffle
+//    * will spin up a managed Ganache instance for you on port 9545 when you
+//    * run `develop` or `test`. You can ask a truffle command to use a specific
+//    * network from the command line, e.g
+//    *
+//    * $ truffle test --network <network-name>
+//    */
+
+//   networks: {
+//     development: {
+//       host: '127.0.0.1',
+//       port: 8000,
+//       network_id: '11155111', // Match any network id
+//     },
     // testnet: {
     //   // host: "localhost",
     //   // port: 8545,
