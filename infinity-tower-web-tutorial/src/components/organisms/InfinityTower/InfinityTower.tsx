bd953ee1418395a90be9710a1d6d27c292de6010
apasingh <104095503+apasingh@users.noreply.github.com>
2024-05-03 21:36:47 -0400

    can't interact with the contract if we are not logged in now

--- infinity-tower-web-tutorial/src/components/organisms/InfinityTower/InfinityTower.tsx
+++ infinity-tower-web-tutorial/src/components/organisms/InfinityTower/InfinityTower.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import { Button } from "@mantine/core";
+import { useEthers, Sepolia } from '@usedapp/core';
 import { FloorCreationModel } from '../FloorCreationModel';
 
 export const InfinityTower = () => {
     const [floorCreationOpened, setFloorCreationOpened] = useState(false);
+    const { account, chainId } = useEthers();
+
     return (
         <>
-        <Button onClick = {() => setFloorCreationOpened(true)} variant='light' radius={8} style={{position: 'fixed', bottom: 42, right: 42}}>
+        <Button onClick = {() => setFloorCreationOpened(true)} variant='light' radius={8} disabled={!account || chainId !== Sepolia.chainId} 
+            style={{position: 'fixed', bottom: 42, right: 42}}>
             Mint a new Floor
         </Button>
         <FloorCreationModel opened={floorCreationOpened} onClose={() => setFloorCreationOpened(false)}></FloorCreationModel>
         </>
     )
-    // <div>Tower</div>
-}
+
+};
