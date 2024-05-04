0bde744dde40fe4be80f99433c7ebaf295de17b5
apasingh <104095503+apasingh@users.noreply.github.com>
2024-05-03 20:36:34 -0400

    now there is a header that encourages metamask connection

--- infinity-tower-web-tutorial/src/App.tsx
+++ infinity-tower-web-tutorial/src/App.tsx
@@ -1,14 +1,21 @@
 // Import MantineProvider from @mantine/core
-import { MantineProvider, Container } from '@mantine/core';
+import { MantineProvider, Container, /*Header,*/ Title} from '@mantine/core';
 import './App.css';
 import { WalletInstallation } from "./components/organisms/WalletInstallation";
+import { WalletConnect } from "./components/organisms/WalletConnect";
 
 function App() {
   const { ethereum } = window as any;
   return (
-    // Wrap your entire app with MantineProvider
+    // Error from before was that I didn't wrap your entire app with MantineProvider
     <MantineProvider>
       <div className="App">
+
+        <div className='header' style={{ height: 60, paddingLeft: 'x1', display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+            <div><Title> Infinity Tower </Title></div>
+            <div><WalletConnect/></div>
+        </div>
+
         {!ethereum ? (
           <Container p="lg">
             <WalletInstallation />
---
+++ infinity-tower-web-tutorial/src/components/organisms/WalletConnect/WalletConnect.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@mantine/core"
+
+export const WalletConnect =() =>{
+    return <Button> Connect to Metamask </Button>
+}
